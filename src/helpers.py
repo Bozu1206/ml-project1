bd1352f60d25f1ba0f87d469482ecbba513121a3
François Dumoncel <francois.dumoncel-kessler@epfl.ch>
2023-10-17 11:33:30 +0200

    update helpers.py

--- src/helpers.py
+++ src/helpers.py
@@ -1,30 +1,69 @@
 """Some helper functions for project 1."""
 import csv
 import numpy as np
+import os
 
 
-def load_csv_data(x_path, y_path, sub_sample=False):
-    """Loads data and returns y (class labels), x (features)"""
-    x = np.genfromtxt(x_path, skip_header=1, delimiter=",")
-    y = np.genfromtxt(y_path, skip_header=1, dtype=int)
+def load_csv_data(data_path, sub_sample=False):
+    """
+    This function loads the data and returns the respectinve numpy arrays.
+    Remember to put the 3 files in the same folder and to not change the names of the files.
 
-    # todo: add sub_sample procedure
-    # todo: check for ids ?
+    Args:
+        data_path (str): datafolder path
+        sub_sample (bool, optional): If True the data will be subsempled. Default to False.
 
-    # Sanity check
-    assert y.shape == (328135,)
-    assert x.shape[0] == y.shape[0] and x.shape[1] == 321
-    return x, y
+    Returns:
+        x_train (np.array): training data
+        x_test (np.array): test data
+        y_train (np.array): labels for training data in format (-1,1)
+        train_ids (np.array): ids of training data
+        test_ids (np.array): ids of test data
+    """
+    y_train = np.genfromtxt(
+        os.path.join(data_path, "y_train.csv"),
+        delimiter=",",
+        skip_header=1,
+        dtype=int,
+        usecols=1,
+    )
+    x_train = np.genfromtxt(
+        os.path.join(data_path, "x_train.csv"), delimiter=",", skip_header=1
+    )
+    x_test = np.genfromtxt(
+        os.path.join(data_path, "x_test.csv"), delimiter=",", skip_header=1
+    )
+
+    train_ids = x_train[:, 0].astype(dtype=int)
+    test_ids = x_test[:, 0].astype(dtype=int)
+    x_train = x_train[:, 1:]
+    x_test = x_test[:, 1:]
+
+    # sub-sample
+    if sub_sample:
+        yb = yb[::50]
+        x_train = x_train[::50]
+        train_ids = train_ids[::50]
+
+    return x_train, x_test, y_train, train_ids, test_ids
 
 
 def create_csv_submission(ids, y_pred, name):
     """
-    Creates an output file in .csv format for submission to Kaggle or AIcrowd
-    Arguments: ids (event ids associated with each prediction)
-               y_pred (predicted class labels)
-               name (string name of .csv output file to be created)
+    This function creates a csv file named 'name' in the format required for a submission in Kaggle or AIcrowd.
+    The file will contain two columns the first with 'ids' and the second with 'y_pred'.
+    y_pred must be a list or np.array of 1 and -1 otherwise the function will raise a ValueError.
+
+    Args:
+        ids (list,np.array): indices
+        y_pred (list,np.array): predictions on data correspondent to indices
+        name (str): name of the file to be created
     """
-    with open(name, "w") as csvfile:
+    # Check that y_pred only contains -1 and 1
+    if not all(i in [-1, 1] for i in y_pred):
+        raise ValueError("y_pred can only contain values -1, 1")
+
+    with open(name, "w", newline="") as csvfile:
         fieldnames = ["Id", "Prediction"]
         writer = csv.DictWriter(csvfile, delimiter=",", fieldnames=fieldnames)
         writer.writeheader()
